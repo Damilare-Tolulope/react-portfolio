6a3877b5654e848d0679c619096a1725cec4bb37
Dee Man <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-02-18 10:05:47 +0100

    Source files

---
+++ src/resumeData.js
@@ -0,0 +1,143 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Ajele Oludamilare",
+    "role": "Web Developer",
+    "whatsapp" : 2348074270190,
+    "linkedinId":"Oluwadamilare Ajele",
+    "twitter": "@_dee_man",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, visiting friends to learn or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"phone",
+          "url":"tel:+2348074270190",
+          "className":"fa fa-phone"
+        },
+        {
+          "name":"gmail",
+          "url":"mailto:damilaretolulope1@gmail.com",
+          "className":"fa fa-envelope"
+        },
+        {
+          "name":"whatsapp",
+          "url":"https://wa.me/qr/ASNPEIWENDXZB1",
+          "className":"fa fa-circle"
+        },
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/oluwadamilare-ajele-803937198/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Damilare-Tolulope",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/_dee_man?s=09",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am self taught Frontend Developer vast in technologies like HTML/CSS, JavaScript, Bootstrap, ReactJs, TypeScript, and currently driving to become a Full Stack Engineer with various technologies. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Nigeria",
+    "website":"https://Ajdee.netlify.app",
+    "education":[
+      {
+        "UniversityName":"Federal University of Technology, Akure",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Sept",
+        "YearOfPassing":"2023",
+        "Achievements":""
+      },
+      // {
+      //   "UniversityName":"Some University",
+      //   "specialization":"Some specialization",
+      //   "MonthOfPassing":"Jan",
+      //   "YearOfPassing":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "work":[
+      {
+        "CompanyName":"Digit Technologies",
+        "specialization":"Frontend Developer",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Transforming UI components into reuseable components"
+      },
+      {
+        "CompanyName":"Oluadepe Farms",
+        "specialization":"React Developer",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2022",
+        "Achievements":"Built reuseable components. Mentored and shared ideas with other developers"
+      }
+    ],
+    "skillsDescription":"Here are my proficient skills",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Typescript"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"NodeJs"
+      },
+      {
+        "skillname":"ExpressJs"
+      },
+      {
+        "skillname":"Git and Github"
+      },
+      {
+        "skillname":"Nextjs"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Pes Convention",
+        "description":"A ",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+    
+  }
+  export default resumeData
